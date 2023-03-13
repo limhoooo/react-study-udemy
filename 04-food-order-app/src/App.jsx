dd0f049d3fd2754c4e09f0dcd8f97bf770ae38d2
lim ho <dlagh123@gmail.com>
2023-03-13 11:01:31 +0900

    Cart context api 추가

--- 04-food-order-app/src/App.jsx
+++ 04-food-order-app/src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvieder';
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
@@ -10,14 +13,16 @@ function App() {
       return state ? false : true
     });
   }
+
+
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart toggleCartHandler={toggleCartHandler} />}
       <Header toggleCartHandler={toggleCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
--- 04-food-order-app/src/components/Cart/Cart.jsx
+++ 04-food-order-app/src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import classes from './Cart.module.css';
 
 const Cart = (props) => {
     const cartItems = <ul className={classes['cart-items']}>
-        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(item => <li>{item.name}</li>)}
+        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(item => <li key={item.id}>{item.name}</li>)}
     </ul>
     return (
         <Modal toggleCartHandler={props.toggleCartHandler}>
