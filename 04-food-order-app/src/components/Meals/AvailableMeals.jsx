b225367451fff906d6aedf91b7f0b613af9d28ee
lim ho <dlagh123@gmail.com>
2023-03-19 15:52:57 +0900

    http 방식으로 변경

--- 04-food-order-app/src/components/Meals/AvailableMeals.jsx
+++ 04-food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,32 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+
+
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+
+    const [meals, setMeals] = useState([])
+    const fetchMeals = useCallback(async () => {
+        const res = await fetch('https://react-test-d49b3-default-rtdb.firebaseio.com//meals.json');
+        const data = await res.json();
+        const loadedMeals = [];
+        for (const key in data) {
+            loadedMeals.push({
+                id: key,
+                name: data[key].name,
+                price: data[key].price,
+                description: data[key].description
+            })
+        }
+        setMeals(loadedMeals);
+    }, [])
+    useEffect(() => {
+        fetchMeals();
+    }, [fetchMeals])
+
+    const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
     return <section className={classes.meals}>
         <Card>
             <ul>
