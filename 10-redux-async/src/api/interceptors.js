65fd82fd397b0a2b5b8eea6e1e0a94b576e2d0b0
lim ho <dlagh123@gmail.com>
2023-03-27 09:03:30 +0900

    인터셉터 주석 주가

--- 10-redux-async/src/api/interceptors.js
+++ 10-redux-async/src/api/interceptors.js
@@ -4,9 +4,11 @@ import { API } from "./index";
 const setUpInterceptors = (store) => {
     API.interceptors.request.use(
         function (config) {
+            // 요청성공시
             return config;
         },
         function (error) {
+            // 요청실패시
             console.log(error);
             return Promise.reject(error);
         },
@@ -14,13 +16,16 @@ const setUpInterceptors = (store) => {
 
     API.interceptors.response.use(
         function (response) {
+            // 응답성공시
             // index.js 에서 넣어준 store 사용가능
             store.dispatch(cartActions.test())
             return response;
         },
         function (error) {
+            // 응답실패시
             console.log(error);
         },
     );
 }
-export default setUpInterceptors
+export default setUpInterceptors;
+
