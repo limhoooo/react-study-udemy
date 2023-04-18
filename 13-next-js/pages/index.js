2f4b4c96e1d85c5b0441e396f8f3c789d6f13a3e
lim ho <dlagh123@gmail.com>
2023-04-18 20:32:31 +0900

    getServerSideProps 추가

--- 13-next-js/pages/index.js
+++ 13-next-js/pages/index.js
@@ -21,7 +21,18 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-// getStaticProps : 빌드 시 데이터를 fetch하여 static 페이지를 생성
+// getServerSideProps : 동적으로 데이터를 요청해야할시 (SSR)
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
+// getStaticProps : 빌드 시 데이터를 fetch하여 static 페이지를 생성 (SSG)
 // 서버측에서 실행되게 하는 함수
 // page 컴포넌트에서만 동작함
 export async function getStaticProps() {
